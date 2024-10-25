2b48ab224df885279bc3366de9e2f36a9b2b164a
Dirk Wieschebrock <dirk.wieschebrock@technotrans.de>
2024-10-25 11:03:01 +0200

    Format string correction

--- CANopenNode_STM32/CO_app_STM32.c
+++ CANopenNode_STM32/CO_app_STM32.c
@@ -167,7 +167,7 @@ canopen_app_resetCommunication() {
                          canopenNodeSTM32->activeNodeID, &errInfo);
     if (err != CO_ERROR_NO && err != CO_ERROR_NODE_ID_UNCONFIGURED_LSS) {
         if (err == CO_ERROR_OD_PARAMETERS) {
-            log_printf("Error: Object Dictionary entry 0x%X\n", errInfo);
+            log_printf("Error: Object Dictionary entry 0x%lX\n", errInfo);
         } else {
             log_printf("Error: CANopen initialization failed: %d\n", err);
         }
@@ -177,7 +177,7 @@ canopen_app_resetCommunication() {
     err = CO_CANopenInitPDO(CO, CO->em, OD, canopenNodeSTM32->activeNodeID, &errInfo);
     if (err != CO_ERROR_NO) {
         if (err == CO_ERROR_OD_PARAMETERS) {
-            log_printf("Error: Object Dictionary entry 0x%X\n", errInfo);
+            log_printf("Error: Object Dictionary entry 0x%lX\n", errInfo);
         } else {
             log_printf("Error: PDO initialization failed: %d\n", err);
         }
